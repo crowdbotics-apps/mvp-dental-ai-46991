f750aa8db3066a65f80acb46fef6b43a746308ae
Crowdbotics <team@crowdbotics.com>
2024-03-13 09:59:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mvp-dental-ai-46991",
+  url: "https://mvp-dental-ai-46991.botics.co",
+  api: "https://mvp-dental-ai-46991.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
